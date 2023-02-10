301f1903dbd43b835ef86feff5ee2ba7e9e471a8
SimplyOneCode <118500380+SimplyOneCode@users.noreply.github.com>
2023-02-10 14:31:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+        var menuList=document.getElementById("menulist");
+        menuList.style.maxHeight="0px";
+        function togglemenu(){
+                if(menuList.style.maxHeight =="0px")
+                    {
+                    menuList.style.maxHeight="450px";
+                    }
+                else
+                    {
+                        menuList.style.maxHeight="0px";
+                    }
+
+        }
+
+        var swiper = new Swiper(".mySwiper", {
+            effect: "coverflow",
+            grabCursor: true,
+            centeredSlides: true,
+            slidesPerView: "auto",
+            coverflowEffect: {
+              rotate: 0,
+              stretch: 0,
+              depth: 100,
+              modifier: 2,
+            },
+            loop:true,
+            pagination: {
+                el: ".swiper-pagination",
+              },
+          });
+
+
+
+// sdasdas/
+
+   
+          
